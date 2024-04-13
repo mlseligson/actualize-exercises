752533cab0f7114a6514024f90463ba4bf7c9b9a
mlseligson <mseligson125@gmail.com>
2024-04-13 14:51:35 -0600

    Week 2 Day 2

---
+++ 221.rb
@@ -0,0 +1,22 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# 3) Start with an array of hashes and create a new array of string values from each hash's :name key.
+#    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+names = []
+[{name: "Criss Angel", age: 27}, {name: "Garrison Harrison", age: 16}].each do |person| 
+    names.push(person[:name])
+end
+
+pp names
+
+# Were you able to easily solve the problem from memory? 
+
+# I had difficulty at first, then decided to spend denote time mindfulness in nature and come back to it.
+
+# If so, describe the next skill you will start to practice tomorrow. 
+# If not, describe the part you got stuck on that requires more practice. 
+# I will continue on classes and enumerate my skills. 
+
